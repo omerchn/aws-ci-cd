3216079aee610ce185e820c05ddb0795d08782bd
omercohen990 <omer.oref@gmail.com>
2022-09-11 17:17:29 +0300

    updated server

--- server/src/server.js
+++ server/src/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use('/', require('./routes/hello'))
+app.use('/api', require('./routes/hello'))
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)

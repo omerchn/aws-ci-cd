27f6668434179163b75da3819fe05f1d068262a1
omercohen990 <omer.oref@gmail.com>
2022-09-11 21:36:18 +0300

    updated client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TS</h1>
       <h1>Hello AWS :)</h1>
       <div className="card">
         <CountBtn />
--- client/src/components/CountBtn.tsx
+++ client/src/components/CountBtn.tsx
@@ -5,7 +5,7 @@ export default function CountBtn() {
   const [count, setCount] = useState(0)
 
   return (
-    <button onClick={() => setCount((count) => count + 1)} data-testid="count-btn">
+    <button onClick={() => setCount((count) => count + 1)} data-testid="count-btn" style={{ outline: 'none' }}>
       count is {count}
     </button>
   )
